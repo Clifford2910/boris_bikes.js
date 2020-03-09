df3c75d4041dcf665609857ab3d467551a0aa499
Alex <aclifford336@gmail.com>
2020-03-09 13:57:27 +0000

    van functionality for distribute added

--- spec/vanSpec.js
+++ spec/vanSpec.js
@@ -1,17 +1,72 @@
 describe('van', function() {
 
   beforeEach(function() {
-    van = new Van()
-    station = new DockingStation(20)
+    van = new Van(10)
     bike = new Bike()
+    bike2 = new Bike()
+    bike3 = new Bike()
+    emptyStation = new DockingStation(20)
   });
 
   describe('collect', function() {
+    it('raises an error when the station is empty', function() {
+      bike.reportBroken()
+      expect(function() {van.collect(emptyStation, bike)}).toThrowError('Station does not contain broken bikes');
+    });
+
+    it('raises an error when the van is full', function() {
+      van.capacity = 1
+      bike.reportBroken()
+      bike2.reportBroken()
+      emptyStation.dock(bike)
+      emptyStation.dock(bike2)
+      van.collect(emptyStation, bike)
+      expect(function() {van.collect(emptyStation, bike2)}).toThrowError('Van is full')
+    });
+
+    it('cannot collect working bikes from DockingStation', function() {
+      emptyStation.dock(bike)
+      expect(function() {van.collect(emptyStation, bike)}).toThrowError('Can only collect broken bikes from stations');
+    });
+
     it('collects broken bikes from DockingStation', function() {
       bike.reportBroken()
-      station.dock(bike)
-      van.collect(station, bike)
+      emptyStation.dock(bike)
+      van.collect(emptyStation, bike)
       expect(van.loadedBrokenBikes).toEqual([bike])
+    });
+  });
+
+  describe('distribute', function() {
+    it('raises an error when the van is empty', function() {
+      expect(function() {van.distribute(emptyStation, bike)}).toThrowError('Van does not contain working bikes')
+    });
+
+    it('cannot distribute broken bikes to stations', function() {
+      bike.reportBroken()
+      emptyStation.dock(bike)
+      van.collect(emptyStation, bike)
+      expect(function() {van.distribute(emptyStation, bike)}).toThrowError('Van does not contain working bikes')
+    })
+
+    // it('distributes working bikes to a station', function() {
+    //   bike.reportBroken()
+    //   emptyStation.dock(bike)
+    //   van.collect(emptyStation, bike)
+    //   bike.fix()
+    //   van.distribute(emptyStation, bike)
+    //   expect(emptyStation.workingBikes).toEqual([bike])
+    // })
+  });
+
+  describe('capacity', function() {
+    it('has a default capacity', function() {
+      expect(van.capacity).toEqual(10)
+    });
+
+    it('can change its default capacity', function() {
+      van.capacity = 20
+      expect(van.capacity).toEqual(20)
     });
   });
 
--- src/van.js
+++ src/van.js
@@ -1,10 +1,44 @@
-function Van() {
+function Van(defaultCapacity = 10) {
+  this.capacity = defaultCapacity;
   this.loadedWorkingBikes = [];
   this.loadedBrokenBikes = [];
 };
 
 Van.prototype.collect = function(station, bike) {
+  if (van.isFull() == true) {
+    throw new Error('Van is full');
+  }
+  if (bike.working == true) {
+    throw new Error('Can only collect broken bikes from stations');
+  }
+  if (station.brokenBikes.length == 0) {
+    throw new Error('Station does not contain broken bikes')
+  }
   this.loadedBrokenBikes.push(bike)
   station.brokenBikes.remove(bike)
   return bike
+};
+
+Van.prototype.distribute = function (station, bike) {
+  if (this.isEmpty()) {
+    throw new Error('Van does not contain working bikes');
+  }
+  station.workingBikes.push(bike)
+  this.loadedWorkingBikes.remove(bike)
+};
+
+Van.prototype.isEmpty = function() {
+  if (this.loadedWorkingBikes.length <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Van.prototype.isFull = function() {
+  if (this.loadedWorkingBikes.length + this.loadedBrokenBikes.length >= this.capacity) {
+    return true;
+  } else {
+    return false;
+  }
 };
